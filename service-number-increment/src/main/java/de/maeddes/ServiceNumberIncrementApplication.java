b247b2bc31efa00421b6d7ea9d47d65c015f3a41
Matthias Haeussler <matthias.haeussler@novatec-gmbh.de>
2017-05-09 15:09:42 +0200

    New code

---
+++ service-number-increment/src/main/java/de/maeddes/ServiceNumberIncrementApplication.java
@@ -0,0 +1,13 @@
+package de.maeddes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ServiceNumberIncrementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceNumberIncrementApplication.class, args);
+	}
+}
