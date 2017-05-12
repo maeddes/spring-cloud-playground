0dfab44333d324dd4adf9e07066f2e3884b78882
Matthias Haeussler <matthias.haeussler@novatec-gmbh.de>
2017-05-12 13:32:38 +0200

    New code

--- service-hello-world/src/main/java/de/maeddes/ServiceHelloWorldApplication.java
+++ service-hello-world/src/main/java/de/maeddes/ServiceHelloWorldApplication.java
@@ -1,8 +1,12 @@
 package de.maeddes;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,4 +15,40 @@ public class ServiceHelloWorldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceHelloWorldApplication.class, args);
 	}
+}
+
+@RestController
+@RefreshScope
+class SimpleController {
+
+    @Value("${CF_INSTANCE_INDEX:0}")
+    private int instance;
+
+    @Value("${message:Hello default}")
+    private String message;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello, bootiful World! Instance: "+instance;
+    }
+
+    @RequestMapping("/crash")
+    public String crash() {
+        System.exit(0);
+        return "Hello, bootiful World!";
+    }
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
+    }
+
+    @RequestMapping("/appName")
+    String getAppName() {
+        return this.appName;
+    }
+
 }
--- service-number-increment/src/main/java/de/maeddes/ServiceNumberIncrementApplication.java
+++ service-number-increment/src/main/java/de/maeddes/ServiceNumberIncrementApplication.java
@@ -1,7 +1,10 @@
 package de.maeddes;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 
@@ -10,4 +13,18 @@ public class ServiceNumberIncrementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceNumberIncrementApplication.class, args);
 	}
+}
+
+@RestController
+class NumberIncrement{
+
+    private int counter = 0;
+
+    @RequestMapping("/")
+    public String getNumber(){
+
+        return "New Number: "+counter++;
+
+    }
+
 }
